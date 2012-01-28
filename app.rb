3adf8b86515c8cd5cde4e93bb722b8305ac664e5
Cody Krieger <cody@codykrieger.com>
2012-01-27 20:37:44 -0500

    Fixed pagination when ?page wasn't present in query string.

--- app.rb
+++ app.rb
@@ -29,13 +29,17 @@ class Pinocchio < Sinatra::Base
     end
     def next_page_url
       return "#" if next_page_url_disabled?
-      url_for_paginated_page(params[:page].to_i + 1)
+      return url_for_paginated_page(params[:page].to_i + 1) if params[:pgae].to_i != 0
+      url_for_paginated_page(2)
     end
     def prev_page_url_disabled?
       (0..1).include? params[:page].to_i
     end
     def next_page_url_disabled?
       params[:page].to_i == (@link_count / PAGE_SIZE).ceil
+    end
+    def page_url_disabled?(index)
+      params[:page].to_i == index || (params[:page].to_i == 0 && index == 1)
     end
     def paginate
       @link_count ||= link_count
