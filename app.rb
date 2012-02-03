17dc215773718e00c659abdbfa9a82c408397950
Cody Krieger <cody@codykrieger.com>
2012-02-03 14:49:46 -0500

    Move session/cookie stuff out into config.ru

--- app.rb
+++ app.rb
@@ -8,15 +8,6 @@ class Pinocchio < Sinatra::Base
   PAGE_SIZE = 15.0
 
   configure do
-    secret_file_path = "#{root}/../session_key"
-    secret_key = "Ouppvx4UKRIJ7zHCDuFEYh7IOwaJ3dIClmROlIzj5Y5RkSVeN2CIZMOar6FxwYL"
-    if File.exist? secret_file_path
-      secret_key = File.read secret_file_path
-    end
-    use Rack::Session::Cookie, key: "_sse_session",
-                               path: '/',
-                               secret: secret_key
-
     register Sinatra::Flash
   end
 
--- config.ru
+++ config.ru
@@ -4,5 +4,14 @@ require 'bundler'
 Bundler.require
 
 require './app.rb'
+
+secret_file_path = "#{root}/../session_key"
+secret_key = "Ouppvx4UKRIJ7zHCDuFEYh7IOwaJ3dIClmROlIzj5Y5RkSVeN2CIZMOar6FxwYL"
+if File.exist? secret_file_path
+  secret_key = File.read secret_file_path
+end
+use Rack::Session::Cookie, key: "_sse_session",
+                           secret: secret_key
+
 run Pinocchio
 
