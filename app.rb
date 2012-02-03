8eb8af41520059a7a1cdd4e1c89ebb26aa1e3bd1
Cody Krieger <cody@codykrieger.com>
2012-02-03 14:30:45 -0500

    Clean up some session key logic

--- app.rb
+++ app.rb
@@ -9,10 +9,9 @@ class Pinocchio < Sinatra::Base
 
   configure do
     secret_file_path = "#{root}/../session_key"
+    secret_key = "Ouppvx4UKRIJ7zHCDuFEYh7IOwaJ3dIClmROlIzj5Y5RkSVeN2CIZMOar6FxwYL"
     if File.exist? secret_file_path
       secret_key = File.read secret_file_path
-    else
-      secret_key = "Ouppvx4UKRIJ7zHCDuFEYh7IOwaJ3dIClmROlIzj5Y5RkSVeN2CIZMOar6FxwYL"
     end
     use Rack::Session::Cookie, key: "_sse_session",
                                path: '/',
