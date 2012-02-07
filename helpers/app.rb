56b4600f05969230c7c0fcf946c266e150bfa87b
Cody Krieger <cody@codykrieger.com>
2012-02-07 13:56:26 -0500

    Get rid of unused helper method

--- helpers/app.rb
+++ helpers/app.rb
@@ -1,30 +1,34 @@
 module Helpers
   module App
-    def asset_path ; request.path == '/' ? '' : request.path ; end
-
     # pagination
 
     def url_for_paginated_page(page)
       url "/?page=#{page}"
     end
+
     def prev_page_url
       return "#" if prev_page_url_disabled?
       url_for_paginated_page(params[:page].to_i - 1)
     end
+
     def next_page_url
       return "#" if next_page_url_disabled?
       return url_for_paginated_page(params[:page].to_i + 1) if params[:pgae].to_i != 0
       url_for_paginated_page(2)
     end
+
     def prev_page_url_disabled?
       (0..1).include? params[:page].to_i
     end
+
     def next_page_url_disabled?
       params[:page].to_i == (@link_count / $PAGE_SIZE).ceil || @link_count <= $PAGE_SIZE
     end
+
     def page_url_disabled?(index)
       params[:page].to_i == index || (params[:page].to_i == 0 && index == 1)
     end
+
     def paginate
       @link_count ||= link_count
 
